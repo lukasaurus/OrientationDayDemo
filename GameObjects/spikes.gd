9ef6dd511f3b8adf2c923e18d22b1d37a1a5ec90
lukasaurus <lukasaurus@gmail.com>
2025-11-14 12:45:26 +1300

    Added Spike Drop

--- GameObjects/spikes.gd
+++ GameObjects/spikes.gd
@@ -1,7 +1,12 @@
-extends Area2D
+extends Node2D
 
 
 
 func _on_body_entered(body: Node2D) -> void:
+	if body is Player:
+		get_tree().reload_current_scene()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Player:
 		get_tree().reload_current_scene()
