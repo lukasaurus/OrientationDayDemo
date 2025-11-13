99c2ceae0dd4ceb575cf804c6b7b1f35c31c7c7a
lukasaurus <lukasaurus@gmail.com>
2025-11-14 09:01:09 +1300

    Updated for 2025

--- GameObjects/player.gd
+++ GameObjects/player.gd
@@ -1,19 +1,22 @@
 extends CharacterBody2D
 class_name Player
-
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -300.0
 const ACCELERATION = 5.0
+var jumps_remaining = 2
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	else:
+		jumps_remaining = 2
+	
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and jumps_remaining > 0:
 		velocity.y = JUMP_VELOCITY
-
+		jumps_remaining -= 1
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
@@ -31,10 +34,12 @@ func _physics_process(delta: float) -> void:
 		$AnimatedSprite2D.flip_h = true
 	if direction > 0:
 		$AnimatedSprite2D.flip_h = false
-
 	move_and_slide()
-
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Player:
 		get_tree().reload_current_scene()
+
+func _on_goal_body_entered(body: Node2D) -> void:
+	if body is Player:
+		get_tree().change_scene_to_file("res://Sprites/UI/end_screen.tscn")
