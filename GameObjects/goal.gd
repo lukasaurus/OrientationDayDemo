6290ccf56d93fe7f6319779fc14ea318e34c0d64
lukasaurus <lukasaurus@gmail.com>
2025-11-14 12:05:11 +1300

    Updated Goal

--- GameObjects/goal.gd
+++ GameObjects/goal.gd
@@ -9,3 +9,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player:
+		get_tree().change_scene_to_file("res://Sprites/UI/end_screen.tscn")
+		pass # Replace with function body.
